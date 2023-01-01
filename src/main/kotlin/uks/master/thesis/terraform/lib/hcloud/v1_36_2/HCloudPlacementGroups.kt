1d0da308c7ea9a4f6f413cd836e61552d7cdfa62
masterchi3f <kestue7@gmail.com>
2023-01-01 17:25:56 +0100

    feat: added hcloud placement groups to terraform lib

---
+++ src/main/kotlin/uks/master/thesis/terraform/lib/hcloud/v1_36_2/HCloudPlacementGroups.kt
@@ -0,0 +1,32 @@
+package uks.master.thesis.terraform.lib.hcloud.v1_36_2
+
+import uks.master.thesis.terraform.syntax.elements.Argument
+import uks.master.thesis.terraform.syntax.elements.Block
+import uks.master.thesis.terraform.syntax.expressions.TfBool
+import uks.master.thesis.terraform.syntax.expressions.TfList
+import uks.master.thesis.terraform.syntax.expressions.TfRef
+import uks.master.thesis.terraform.syntax.expressions.TfString
+
+object HCloudPlacementGroups {
+    private const val HCLOUD_PLACEMENT_GROUPS: String = "hcloud_placement_groups"
+    private const val WITH_SELECTOR: String = "with_selector"
+    private const val MOST_RECENT: String = "most_recent"
+    private const val PLACEMENT_GROUPS: String = "placement_groups"
+
+    class DataSource private constructor(block: Block, self: String):
+        uks.master.thesis.terraform.syntax.elements.blocks.DataSource(block, self) {
+        val placementGroups get() = TfRef<TfList>(reference(PLACEMENT_GROUPS))
+
+        class Builder: GBuilder<Builder>() {
+            private val withSelectorBuilder: Argument.Builder = Argument.Builder().name(WITH_SELECTOR)
+            private val mostRecentBuilder: Argument.Builder = Argument.Builder().name(MOST_RECENT)
+            init { dataSource(HCLOUD_PLACEMENT_GROUPS) }
+
+            fun withSelector(selector: String) = apply { addElement(withSelectorBuilder.value(selector).build()) }
+            fun withSelector(ref: TfRef<TfString>) = apply { addElement(withSelectorBuilder.raw(ref.toString()).build()) }
+            fun mostRecent(mostRecent: Boolean) = apply { addElement(mostRecentBuilder.value(mostRecent).build()) }
+            fun mostRecent(ref: TfRef<TfBool>) = apply { addElement(mostRecentBuilder.raw(ref.toString()).build()) }
+            override fun build() = DataSource(buildBlock(), buildSelf())
+        }
+    }
+}
