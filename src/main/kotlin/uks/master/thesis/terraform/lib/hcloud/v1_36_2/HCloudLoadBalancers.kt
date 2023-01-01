14ac9ae76b221291789d603f1730a44a9c088f9e
masterchi3f <kestue7@gmail.com>
2023-01-01 17:07:44 +0100

    feat: added hcloud load balancers to terraform lib

---
+++ src/main/kotlin/uks/master/thesis/terraform/lib/hcloud/v1_36_2/HCloudLoadBalancers.kt
@@ -0,0 +1,27 @@
+package uks.master.thesis.terraform.lib.hcloud.v1_36_2
+
+import uks.master.thesis.terraform.syntax.elements.Argument
+import uks.master.thesis.terraform.syntax.elements.Block
+import uks.master.thesis.terraform.syntax.expressions.TfList
+import uks.master.thesis.terraform.syntax.expressions.TfRef
+import uks.master.thesis.terraform.syntax.expressions.TfString
+
+object HCloudLoadBalancers {
+    private const val HCLOUD_LOAD_BALANCER: String = "hcloud_load_balancer"
+    private const val WITH_SELECTOR: String = "with_selector"
+    private const val LOAD_BALANCERS: String = "load_balancers"
+
+    class DataSource private constructor(block: Block, self: String):
+        uks.master.thesis.terraform.syntax.elements.blocks.DataSource(block, self) {
+        val loadBalancers get() = TfRef<TfList>(reference(LOAD_BALANCERS))
+
+        class Builder: GBuilder<Builder>() {
+            private val withSelectorBuilder: Argument.Builder = Argument.Builder().name(WITH_SELECTOR)
+            init { dataSource(HCLOUD_LOAD_BALANCER) }
+
+            fun withSelector(selector: String) = apply { addElement(withSelectorBuilder.value(selector).build()) }
+            fun withSelector(ref: TfRef<TfString>) = apply { addElement(withSelectorBuilder.raw(ref.toString()).build()) }
+            override fun build() = DataSource(buildBlock(), buildSelf())
+        }
+    }
+}
