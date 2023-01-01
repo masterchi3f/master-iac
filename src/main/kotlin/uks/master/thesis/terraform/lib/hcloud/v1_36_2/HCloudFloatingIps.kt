ff33e07d75639f4eb6c751657f918e3bd6bbc254
masterchi3f <kestue7@gmail.com>
2023-01-01 16:31:18 +0100

    feat: added hcloud floating ips to terraform lib

---
+++ src/main/kotlin/uks/master/thesis/terraform/lib/hcloud/v1_36_2/HCloudFloatingIps.kt
@@ -0,0 +1,27 @@
+package uks.master.thesis.terraform.lib.hcloud.v1_36_2
+
+import uks.master.thesis.terraform.syntax.elements.Argument
+import uks.master.thesis.terraform.syntax.elements.Block
+import uks.master.thesis.terraform.syntax.expressions.TfList
+import uks.master.thesis.terraform.syntax.expressions.TfRef
+import uks.master.thesis.terraform.syntax.expressions.TfString
+
+object HCloudFloatingIps {
+    private const val HCLOUD_FLOATING_IPS: String = "hcloud_floating_ips"
+    private const val WITH_SELECTOR: String = "with_selector"
+    private const val FLOATING_IPS: String = "floating_ips"
+
+    class DataSource private constructor(block: Block, self: String):
+        uks.master.thesis.terraform.syntax.elements.blocks.DataSource(block, self) {
+        val floatingIps get() = TfRef<TfList>(reference(FLOATING_IPS))
+
+        class Builder: GBuilder<Builder>() {
+            private val withSelectorBuilder: Argument.Builder = Argument.Builder().name(WITH_SELECTOR)
+            init { dataSource(HCLOUD_FLOATING_IPS) }
+
+            fun withSelector(selector: String) = apply { addElement(withSelectorBuilder.value(selector).build()) }
+            fun withSelector(ref: TfRef<TfString>) = apply { addElement(withSelectorBuilder.raw(ref.toString()).build()) }
+            override fun build(): DataSource = DataSource(buildBlock(), buildSelf())
+        }
+    }
+}
