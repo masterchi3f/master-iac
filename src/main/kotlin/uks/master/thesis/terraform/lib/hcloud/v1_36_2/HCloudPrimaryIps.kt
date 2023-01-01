77ea92cd5a4dc5efb4ebee922cf4afa4e19c3582
masterchi3f <kestue7@gmail.com>
2023-01-01 17:28:36 +0100

    feat: added hcloud primary ips to terraform lib

---
+++ src/main/kotlin/uks/master/thesis/terraform/lib/hcloud/v1_36_2/HCloudPrimaryIps.kt
@@ -0,0 +1,27 @@
+package uks.master.thesis.terraform.lib.hcloud.v1_36_2
+
+import uks.master.thesis.terraform.syntax.elements.Argument
+import uks.master.thesis.terraform.syntax.elements.Block
+import uks.master.thesis.terraform.syntax.expressions.TfNumber
+import uks.master.thesis.terraform.syntax.expressions.TfRef
+import uks.master.thesis.terraform.syntax.expressions.TfString
+
+object HCloudPrimaryIps {
+    private const val HCLOUD_PRIMARY_IPS: String = "hcloud_primary_ips"
+    private const val WITH_SELECTOR: String = "with_selector"
+    private const val PRIMARY_IPS: String = "primary_ips"
+
+    class DataSource private constructor(block: Block, self: String):
+        uks.master.thesis.terraform.syntax.elements.blocks.DataSource(block, self) {
+        val primaryIps get() = TfRef<TfNumber>(reference(PRIMARY_IPS))
+
+        class Builder: GBuilder<Builder>() {
+            private val withSelectorBuilder: Argument.Builder = Argument.Builder().name(WITH_SELECTOR)
+            init { dataSource(HCLOUD_PRIMARY_IPS) }
+
+            fun withSelector(selector: String) = apply { addElement(withSelectorBuilder.value(selector).build()) }
+            fun withSelector(ref: TfRef<TfString>) = apply { addElement(withSelectorBuilder.raw(ref.toString()).build()) }
+            override fun build() = DataSource(buildBlock(), buildSelf())
+        }
+    }
+}
