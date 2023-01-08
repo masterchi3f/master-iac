1a73858aa4a18c5bfa39960f9a68706a1fee26a0
masterchi3f <kestue7@gmail.com>
2023-01-08 14:02:10 +0100

    feat: input variable generic changes

--- src/main/kotlin/uks/master/thesis/terraform/syntax/elements/blocks/InputVariable.kt
+++ src/main/kotlin/uks/master/thesis/terraform/syntax/elements/blocks/InputVariable.kt
@@ -6,9 +6,12 @@ import uks.master.thesis.terraform.syntax.Expression
 import uks.master.thesis.terraform.syntax.Identifier
 import uks.master.thesis.terraform.syntax.elements.Argument
 import uks.master.thesis.terraform.syntax.elements.Block
+import uks.master.thesis.terraform.syntax.expressions.TfBool
 import uks.master.thesis.terraform.syntax.expressions.TfList
 import uks.master.thesis.terraform.syntax.expressions.TfMap
+import uks.master.thesis.terraform.syntax.expressions.TfNumber
 import uks.master.thesis.terraform.syntax.expressions.TfRef
+import uks.master.thesis.terraform.syntax.expressions.TfString
 
 class InputVariable<T : Expression> private constructor(
     private val block: Block,
@@ -24,24 +27,40 @@ class InputVariable<T : Expression> private constructor(
     val reference get() = TfRef<T>("$VAR.$self")
     val name get() = self
 
-    class Builder<T: Expression> {
+    class Builder<T: Expression>(private val expClass: Class<T>) {
+        companion object {
+            inline operator fun <reified T : Expression>invoke() = Builder(T::class.java)
+        }
         private val blockBuilder: Block.Builder = Block.Builder()
         private val defaultBuilder: Argument.Builder = Argument.Builder().name(DEFAULT)
         private val sensitiveBuilder: Argument.Builder = Argument.Builder().name(SENSITIVE)
         private lateinit var _name: Identifier
+        init { checkAtInit(listOf(TfBool(true), TfNumber(0.0), TfString(""), TfList.Builder().build(), TfMap.Builder().build())) }
 
         fun name(name: String) = apply { preventDupName(); _name = Identifier(name) }
         fun defaultAsNull() = apply { blockBuilder.addElement(defaultBuilder.nullValue().build()) }
-        fun default(bool: Boolean) = apply { blockBuilder.addElement(defaultBuilder.value(bool).build()) }
-        fun default(number: Double) = apply { blockBuilder.addElement(defaultBuilder.value(number).build()) }
-        fun default(string: String) = apply { blockBuilder.addElement(defaultBuilder.value(string).build()) }
-        fun default(list: TfList) = apply { blockBuilder.addElement(defaultBuilder.value(list).build()) }
-        fun default(map: TfMap) = apply { blockBuilder.addElement(defaultBuilder.value(map).build()) }
+        fun default(bool: Boolean) = apply { checkType(TfBool(bool)); blockBuilder.addElement(defaultBuilder.value(bool).build()) }
+        fun default(number: Double) = apply { checkType(TfNumber(number)); blockBuilder.addElement(defaultBuilder.value(number).build()) }
+        fun default(string: String) = apply { checkType(TfString(string)); blockBuilder.addElement(defaultBuilder.value(string).build()) }
+        fun default(list: TfList) = apply { checkType(list); blockBuilder.addElement(defaultBuilder.value(list).build()) }
+        fun default(map: TfMap) = apply { checkType(map); blockBuilder.addElement(defaultBuilder.value(map).build()) }
         fun sensitive() = apply { blockBuilder.addElement(sensitiveBuilder.value(true).build()) }
         fun build() = InputVariable<T>(
             blockBuilder.type(VARIABLE).addLabel(_name.toString()).build(),
             _name.toString()
         )
+
+        private fun checkType(expression: Expression) {
+            if (!expClass.isAssignableFrom(expression.javaClass)) {
+                throw IllegalArgumentException("$expression is not from class ${expClass.name}")
+            }
+        }
+
+        private fun checkAtInit(expressions: List<Expression>) {
+            if (expressions.none { expClass.isAssignableFrom(it.javaClass) }) {
+                throw IllegalArgumentException("${expClass.name} can't be created from any of the classes of $expressions")
+            }
+        }
 
         private fun preventDupName() {
             if (::_name.isInitialized) throw IllegalArgumentException("name was already set to $_name!")
