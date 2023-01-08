6f534a2f63e5af79106863bdb020aa1fbb23b0d0
masterchi3f <kestue7@gmail.com>
2023-01-08 13:17:42 +0100

    feat: added better toString method to TfNumber (for converting e.g. 5.0 to 5)

--- src/main/kotlin/uks/master/thesis/terraform/syntax/expressions/TfNumber.kt
+++ src/main/kotlin/uks/master/thesis/terraform/syntax/expressions/TfNumber.kt
@@ -1,9 +1,10 @@
 package uks.master.thesis.terraform.syntax.expressions
 
+import kotlin.math.floor
 import uks.master.thesis.terraform.syntax.Expression
 
 class TfNumber(
     private val number: Double
 ): Expression {
-    override fun toString(): String = number.toString()
+    override fun toString(): String = String.format(if (floor(number) == number) "%.0f" else "%s", number)
 }
